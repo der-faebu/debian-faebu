2dfc633c360763fb4e91f3ca1f0a42884e4f8cb3
gfa@root.ch <gfa@root.ch>
2023-11-10 16:46:49 +0100

    added docker

--- server/install.sh
+++ server/install.sh
@@ -17,9 +17,9 @@ apt upgrade -y
 apt install nala -y
 
 # Installing Essential Programs 
-nala install openssh-server ufw ca-certificates curl gnupg
+nala install openssh-server ufw ca-certificates curl gnupg -y
 
-# Install Docker engine
+# Docker
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do nala remove $pkg; done
 ## Add Docker's official GPG key:
 nala update
@@ -27,12 +27,19 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Add the repository to Apt sources:
+## Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 nala update
 
+## Install Docker engine
+nala install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+## Add current user to docker group
+usermod -aG docker $username
+
+##
 # Use nala
 bash scripts/usenala
