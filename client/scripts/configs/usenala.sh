4f00cd5bf6f1f78d3482ef6c8764f77a0706538a
faebu <fabian@gygax.tech>
2023-11-22 12:46:45 +0100

    run as sudo

--- client/scripts/configs/usenala.sh
+++ client/scripts/configs/usenala.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./$(basename "$0")" 2>&1
+  exit 1
+fi
+
 username=$(id -u -n 1000)
 # Configure bash to use nala wrapper instead of apt
 usenala="/home/$username/.use-nala"
