b554c659b204dabb913c62e9d02a58dba5bf367d
faebu <fabian@gygax.tech>
2023-12-23 10:25:16 +0100

    added kde-package

--- client/scripts/configs/set_configs.sh
+++ client/scripts/configs/set_configs.sh
@@ -4,8 +4,11 @@ username=$(id -u -n 1000)
 # Making .config and Moving config files and background to Pictures
 builddir=$1
 
+echo "${YELLOW} Builddir: $builddir"
+
 cd $builddir
 
+mkdir /home/$username/.config
 cp -R $builddir/dotconfig/config_files/* /home/$username/.config/
 
 cp $builddir/.bashrc /home/$username/.bashrc
