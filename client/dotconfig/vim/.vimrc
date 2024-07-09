59f53a0b2756c31327998d750ac097800adc4565
Fabian Gygax <46447379+der-faebu@users.noreply.github.com>
2024-07-09 22:36:38 +0200

    vimrc

---
+++ client/dotconfig/vim/.vimrc
@@ -0,0 +1,8 @@
+set number
+set relativenumber
+set tabstop=4
+set shiftwidth=4
+set autoindent
+colorscheme elflord
+syntax on
+filetype plugin indent on
--- client/scripts/configs/set_configs.sh
+++ client/scripts/configs/set_configs.sh
@@ -20,6 +20,8 @@ cp -R $builddir/dotconfig/config_files/* /home/$username/.config/
 cp -f $builddir/dotconfig/bash/.bashrc /home/$username/.bashrc
 cp -f $builddir/dotconfig/bash/.bash_logout /home/$username/.bash_logout
 cp -f $builddir/dotconfig/bash/.bash_aliases /home/$username/.bash_aliases
+cp -f $builddir/dotconfig/vim/.vimrc /home/$username/.vimrc
+
 
 usermod -aG libvirt $username
 usermod -aG kvm $username
